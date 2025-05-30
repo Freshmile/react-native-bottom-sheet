8308136de848ca2b26f7d3165dda8f716e204978
Guillaume HITIER <guillaume.hitier@freshmile.com>
2025-05-30 17:15:04 +0200

    revert: unignore lib/ (build output)

---
+++ lib/typescript/components/bottomSheetHandle/styles.d.ts
@@ -0,0 +1,14 @@
+export declare const styles: {
+    container: {
+        padding: number;
+        cursor: string;
+    };
+    indicator: {
+        alignSelf: "center";
+        width: number;
+        height: number;
+        borderRadius: number;
+        backgroundColor: string;
+    };
+};
+//# sourceMappingURL=styles.d.ts.map
