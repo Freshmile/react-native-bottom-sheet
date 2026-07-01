2b0f40f6981e28fbe658eb8242038977555601e2
Guillaume HITIER <guillaume.hitier@freshmile.com>
2026-07-01 15:41:26 +0200

    fix: flashlist props
    cherry-picked commit : 377d2de0beb478d0bdf328bf18a9c1717dc201d1

--- src/components/bottomSheetScrollable/BottomSheetFlashList.tsx
+++ src/components/bottomSheetScrollable/BottomSheetFlashList.tsx
@@ -1,13 +1,20 @@
-// @ts-ignore
-import type { FlashListProps } from '@shopify/flash-list';
 import React, { forwardRef, memo, type Ref, useMemo } from 'react';
-import type { ScrollViewProps } from 'react-native';
+import type { FlatListProps, ScrollViewProps } from 'react-native';
 import type { AnimatedProps } from 'react-native-reanimated';
 import BottomSheetScrollView from './BottomSheetScrollView';
 import type {
-  BottomSheetScrollViewMethods,
   BottomSheetScrollableProps,
+  BottomSheetScrollViewMethods,
 } from './types';
+
+/**
+ * Minimal subset of FlashListProps needed for BottomSheetFlashList.
+ * Defined locally to avoid requiring @shopify/flash-list as a dependency,
+ * since the runtime import is optional (try/catch require).
+ */
+interface FlashListProps<T> extends FlatListProps<T> {
+  estimatedItemSize?: number;
+}
 
 let FlashList: {
   FlashList: React.FC;
@@ -51,10 +58,10 @@ const BottomSheetFlashListComponent = forwardRef<
   const renderScrollComponent = useMemo(
     () =>
       forwardRef<BottomSheetScrollViewMethods, ScrollViewProps>(
-        // @ts-ignore
+        // @ts-expect-error
         ({ data, ...props }, ref) => {
           return (
-            // @ts-ignore
+            // @ts-expect-error
             <BottomSheetScrollView
               ref={ref}
               {...props}
